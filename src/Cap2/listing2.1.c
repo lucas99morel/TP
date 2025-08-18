3a15709818a5aaa8edc4028fa15e64f35403069c
lucas99morel <lucas99morel@gmail.com>
2025-08-18 16:43:53 -0300

    Uso de argc y argv

---
+++ src/Cap2/listing2.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    printf("El nombre de este programa es '%s',\n",argv[0]);
+    printf("Este programa fue invocado con %d argumentos.\n",argc-1);
+
+    if (argc > 1){
+        int i;
+        printf("Los argumentos son:\n");
+        for (i=1;i<argc;++i){
+            printf("    %s\n",argv[i]);
+        }
+    }
+
+    return 0;
+}
