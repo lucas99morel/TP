836fb9a67931a9df396e8be7ed6c13d41ade5cb2
lucas99morel <lucas99morel@gmail.com>
2025-08-23 17:31:42 -0300

    Uso de mkstemp

---
+++ src/Cap2/listing2.5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+typedef int temp_file_handle;
+
+temp_file_handle write_temp_file(char* buffer, size_t length){
+    /* Archivo unico*/
+    char temp_filename[] = "/tmp/temp_file.XXXXXX";
+    int fd = mkstemp(temp_filename);
+
+    /*Eliminar el archivo del sistema de archivos inmediatamente*/
+    unlink(temp_filename);
+
+    /*Escribir primero el tamaño de los datos*/
+    write(fd,&length,sizeof(length));
+
+    /*Escribir el contenido del buffer*/
+    write(fd,buffer,length);
+
+    return fd;
+}
+
+char* read_temp_file(temp_file_handle temp_file, size_t* lenght){
+    char* buffer;
+    int fd = temp_file;
+    lseek(fd,0,SEEK_SET);
+    read(fd,lenght,sizeof(*lenght));
+    close(fd);
+    return buffer;
+}
