4dfb4e940e49e92e8790b4f4c15bea5c4fae4ff5
lucas99morel <lucas99morel@gmail.com>
2025-08-24 22:43:20 -0300

    Uso de fork con exec

---
+++ src/Cap3/listing3.4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int spaw(char* program, char** arg_list){
+    pid_t child_pid;
+
+    child_pid = fork();
+    if (child_pid != 0){
+        return child_pid;
+    }
+    else{
+        execvp(program,arg_list);
+        fprintf(stderr,"Ocurrio un error en execvp\n");
+        abort();
+    }
+}
+
+int main(){
+    char* arg_list[] = {"ls","l","/",NULL};
+    spaw("ls",arg_list);
+
+    printf("Programa main terminado.\n");
+    return 0;
+}
