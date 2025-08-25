226664883667088759dfc6b4d13822c1199b0989
lucas99morel <lucas99morel@gmail.com>
2025-08-24 22:54:22 -0300

    Uso de un manejador de señal

---
+++ src/Cap3/listing3.5.c
@@ -0,0 +1,22 @@
+#define _POSIX_C_SOURCE 200809L
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+sig_atomic_t sigusr1_count = 0;
+
+void handler(int signal_number){
+    ++sigusr1_count;
+}
+
+int main(){
+    struct sigaction sa;
+    memset(&sa,0,sizeof(sa));
+    sa.sa_handler = &handler;
+    sigaction(SIGUSR1,&sa,NULL);
+
+    printf("SIGUSR1 fue lanzada %d veces\n",sigusr1_count);
+    return 0;
+}   
