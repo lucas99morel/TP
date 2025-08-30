a0b8a7420a557a9c3fac0131c50123cbe35719ad
lucas99morel <lucas99morel@gmail.com>
2025-08-30 20:39:06 -0300

    Inicializar semaforos

---
+++ src/Cap5/listing5.3.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+
+union semun{
+    int val;
+    struct semid_ds *buf;
+    unsigned short int *array;
+    struct seminfo *__buf;
+};
+
+int binary_semaphore_initialize(int semid){
+    union semun argument;
+    unsigned short values[1];
+    values[0] = 1;
+    argument.array = values;
+    return semctl(semid,0,SETALL,argument);
+}
