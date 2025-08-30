8a1daf473ecc77680b9d45a1a6a955cab51b5c92
lucas99morel <lucas99morel@gmail.com>
2025-08-30 17:34:51 -0300

    Mutex

---
+++ src/Cap4/listing4.11.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <malloc.h>
+#include <pthread.h>
+
+struct job {
+    struct job* next;
+};
+
+struct job* job_queue;
+pthread_mutex_t job_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void process_job(struct job* j) {
+    /* Codigo de process_job */
+}
+
+void* thread_function(void* arg){
+    while(1){
+        struct job* next_job;
+        pthread_mutex_lock(&job_queue_mutex);
+
+        if (job_queue == NULL){
+            next_job = NULL;
+        }
+        else{
+            next_job = job_queue;
+            job_queue = job_queue->next;
+            process_job(next_job);
+        }
+
+        pthread_mutex_unlock(&job_queue_mutex);
+        if (next_job == NULL){
+            break;
+        }
+
+        process_job(next_job);
+        free(next_job);
+
+    }
+
+    return NULL;
+}
+
+int main(){
+    return 0;
+}
