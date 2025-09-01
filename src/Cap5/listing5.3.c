686b02b25e9e34982987d0c57832c0710472adfa
lucas99morel <lucas99morel@gmail.com>
2025-08-31 22:55:13 -0300

    Inicializar semaforos

--- src/Cap5/listing5.3.c
+++ src/Cap5/listing5.3.c
@@ -1,6 +1,8 @@
 #include <sys/types.h>
 #include <sys/sem.h>
 #include <sys/ipc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 union semun{
     int val;
@@ -15,4 +17,22 @@ int binary_semaphore_initialize(int semid){
     values[0] = 1;
     argument.array = values;
     return semctl(semid,0,SETALL,argument);
+}
+
+int main(){
+    int semid = semget(IPC_PRIVATE, 1, 0666 | IPC_CREAT);
+    if(semid == -1){
+        perror("semget");
+        exit(1);
+    }
+
+    if(binary_semaphore_initialize(semid) == -1){
+        perror("semctl - initialize");
+        exit(1);
+    }
+
+    printf("Semáforo inicializado en 1\n");
+
+    semctl(semid, 0, IPC_RMID); 
+    return 0;
 }
