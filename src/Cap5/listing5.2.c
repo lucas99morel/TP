76b3def3de193169fac8751c0063d59fece56114
lucas99morel <lucas99morel@gmail.com>
2025-08-31 22:54:26 -0300

    Asignar/desasignar Semaforos

--- src/Cap5/listing5.2.c
+++ src/Cap5/listing5.2.c
@@ -1,6 +1,9 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include<stdio.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 union semun{
     int val;
@@ -18,6 +21,25 @@ int binary_semaphore_desallocate(int semid){
     return semctl(semid,1,IPC_RMID,ignored_argument);
 }
 
+int binary_semaphore_initialize(int semid){
+    union semun argument;
+    unsigned short values[1];
+    values[0] = 1;
+    argument.array = values;
+    return semctl(semid,0,SETALL,argument);
+}
+
 int main(){
+    int semid = binary_semaphore_allocation(IPC_PRIVATE,0666|IPC_CREAT);
+    binary_semaphore_initialize(semid);
+
+    struct sembuf sb = {0,-1,0};
+    semop(semid,&sb,1);
+    printf("Seccion critica...\n");
+    sleep(2);
+    sb.sem_op = 1;
+    semop(semid,&sb,1);
+
+    binary_semaphore_desallocate(semid);
     return 0;
 }
